6c83bb263a99fa49f3ad1d0b2bb88203e0439fa4
enricorobazza <enricorobazzi@gmail.com>
2019-09-25 23:21:02 -0300

    Cors configurated global

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const usersDao = require('./database/dao/users');
 const User = require('./models/user');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
 
 require('./controllers/authController')(app);
 require('./controllers/projectController')(app);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,18 +5,25 @@ module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if(!authHeader)
+    {
+        console.log("No token provided");
         return res.status(401).send({error: 'No token provided'});
-
+    }
 
     const parts = authHeader.split(' ');
 
-    if(!parts.length === 2)
+    if(!parts.length === 2){
+        console.log("Token error");
         return res.status(401).send({error: 'Token error'});
+    }
 
     const [scheme, token] = parts;
 
     if(!/^Bearer$/i.test(scheme))
+    {
+        console.log("Token error");
         return res.status(401).send({error: 'Token malformatted'});
+    }
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
         if(err) return res.status(401).send({error: "Token invalid"});
